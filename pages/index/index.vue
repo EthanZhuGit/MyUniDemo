549e17b6facd60a4a038f4e392a16ce923b36cc3
zhuyixin <zhu_yixin@qq.com>
2022-07-30 11:39:33 +0800

    图片文件不能防止static/h5 文件夹

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -4,6 +4,23 @@
 		<view class="text-area">
 			<text class="title">{{title}}</text>
 		</view>
+		<u--form labelPosition="top"  >
+			<u-form-item label="姓名" borderBottom  :required="true">
+				<input/>
+			</u-form-item>
+		</u--form>
+
+		<u-popup mode="center" :show="showPop" :closeOnClickOverlay="true" @close="popViewClose()">
+			<scroll-view :scroll-y="true" class='pop_view'>
+				<view>12345678</view>
+				<view v-for="(item,index) in 10">
+					<view>{{item}}</view>
+					<!-- 		<view v-for="(item2,index2) in item.subList" >
+						<view>{{item2.data}}</view>
+					</view> -->
+				</view>
+			</scroll-view>
+		</u-popup>
 	</view>
 </template>
 
@@ -11,24 +28,87 @@
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				title: 'Hello',
+				showPop: false,
+				list: [{
+					id: 1,
+					name: 'item1',
+					subList: [{
+						index: 1,
+						data: 'data1'
+					}, {
+						index: 2,
+						data: 'data2'
+					}]
+				}, {
+					id: 2,
+					name: 'item2',
+					subList: [{
+						index: 3,
+						data: 'data3'
+					}, {
+						index: 4,
+						data: 'data4'
+					}, {
+						index: 5,
+						data: 'data5'
+					}]
+				}, {
+					id: 3,
+					name: 'item3',
+					subList: [{
+						index: 6,
+						data: 'data6'
+					}, {
+						index: 7,
+						data: 'data7'
+					}, {
+						index: 8,
+						data: 'data8'
+					}]
+				}, {
+					id: 4,
+					name: 'item4',
+					subList: [{
+						index: 9,
+						data: 'data9'
+					}]
+				}]
 			}
 		},
 		onLoad() {
 
 		},
 		methods: {
+			imgClick() {
+				this.showPop = true;
+			},
+			itemClick(e) {
+				console.log("itemClick", e);
+			},
+			subItemClick(e) {
+				console.log("subItemClick", e);
+			},
+			popViewClose() {
 
+			}
 		}
 	}
 </script>
 
 <style>
+	.pop_view {
+		width: 550rpx;
+		max-height: 50vh;
+	}
+
 	.content {
 		display: flex;
 		flex-direction: column;
 		align-items: center;
 		justify-content: center;
+		background-color: antiquewhite;
+		width: 100%;
 	}
 
 	.logo {
